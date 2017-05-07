bf2f97de12eee6d52f2138c50d82c59655456e39
Dmitry Smertin <smertindmi@mail.ru>
2017-05-07 16:09:20 +0300

    Значения символов

---
+++ TablesOfPolynominal/TablesOfPolynominal/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+void main()
+{
+	cout << "a - " <<(int)('a')<<endl;
+	cout << "z - " << (int)('z') << endl;
+	cout << "A - " << (int)('A') << endl;
+	cout << "Z - " << (int)('Z') << endl;
+	cout << "0 - " << (int)('0') << endl;
+	cout << "1 - " << (int)('1') << endl;
+	cout << "9 - " << (int)('9') << endl;
+}
